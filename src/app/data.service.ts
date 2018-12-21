b342d4395863d38e909754b8779094b993057788
kocic11 <25288526+kocic11@users.noreply.github.com>
2018-12-21 09:48:13 -0500

    Updated

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,18 +1,6 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { HttpHeaders } from '@angular/common/http';
-import { HttpParams } from "@angular/common/http";
 import { Observable } from "rxjs";
-import * as _ from 'lodash';
-import { map } from "rxjs/operators";
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With',
-    'Access-Control-Allow-Methods': 'GET, PUT, POST'
-  })
-};
 
 export interface Patient {
   name: {
@@ -42,7 +30,6 @@ const params = new HttpParams()
   providedIn: 'root'
 })
 export class DataService {
-  patients$: Observable<Patient[]>;
   constructor(private http: HttpClient) { }
 
   getPatients(): Observable<Patient[]> {
